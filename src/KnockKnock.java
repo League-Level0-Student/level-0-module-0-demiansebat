3febea3ffefc6ce4419abf522e444170108bc151
demiansebat <league@WTS14-4.attlocal.net>
2018-09-14 18:25:30 -0700

    Greeter and Knock Knock

--- src/KnockKnock.java
+++ src/KnockKnock.java
@@ -1,6 +1,11 @@
+import javax.swing.JOptionPane;
 
-public class KnockKnock {public static void main(String[] args) {
-	
-}
-
+public class KnockKnock {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Knock-Knock");
+		JOptionPane.showMessageDialog(null, "Whoes there?");
+		JOptionPane.showMessageDialog(null, "nobody");
+		JOptionPane.showMessageDialog(null, "nobody who?");
+		JOptionPane.showMessageDialog(null, "nobody's listening my jokes are way too depressing :(");
+	}
 }
