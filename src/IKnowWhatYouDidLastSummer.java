010de6029771c8dd4519bd898c0447e53fa5bbbc
demiansebat <league@WTS14-4.attlocal.net>
2018-09-28 17:45:58 -0700

    Finished I know what you did last summer

--- src/IKnowWhatYouDidLastSummer.java
+++ src/IKnowWhatYouDidLastSummer.java
@@ -1,4 +1,8 @@
+import javax.swing.JOptionPane;
 
 public class IKnowWhatYouDidLastSummer {
-
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("What is your name?");
+		JOptionPane.showMessageDialog(null, "I know that you played soccer last summer " + name);
+	}
 }
